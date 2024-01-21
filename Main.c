d9d95449db56fda5d2957927c699c1dcb6c274ad
Mateusz <mateusz@tulip.lol>
2024-01-21 17:17:38 +0100

    Led blink code added

---
+++ Main.c
@@ -0,0 +1,22 @@
+//Author mztulip
+//mateusz@tulip.lol
+
+#include "CH57x_common.h"
+
+int main()
+{       
+	/* PB0-LED */
+	GPIOB_ModeCfg( GPIO_Pin_0, GPIO_ModeOut_PP_20mA );
+	while(1)
+	{
+		
+		mDelaymS(100);
+		GPIOB_SetBits( GPIO_Pin_0 ); 
+		mDelaymS(100);
+		GPIOB_ResetBits( GPIO_Pin_0 );
+	}
+}
+
+
+
+
